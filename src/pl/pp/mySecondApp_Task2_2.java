7f1b86394d5cf1ff4d8b2f6d1090c23f65ef45c3
Gochmyrat <akmyradowgochmyrat9@gmail.com>
2024-03-19 22:57:22 +0100

    First save to Lab2 repository

---
+++ src/pl/pp/mySecondApp_Task2_2.java
@@ -0,0 +1,12 @@
+package pl.pp;
+
+public class mySecondApp_Task2_2 {
+    public static void main(String[] args) {
+        int myage = 24;
+        int secondsinoneyear = 365*24*60*60;
+        int myageinseconds = secondsinoneyear * myage;
+
+        System.out.println("My age in seconds = " + myageinseconds);
+    }
+}
+
